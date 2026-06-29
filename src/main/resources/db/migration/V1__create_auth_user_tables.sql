a55b15d24dbae74595849cd5c28a31cbf1066acb
AnonimProgrammer <zephyr11ff@gmail.com>
2026-06-29 22:51:55 +0400

    feat: Add users table to initial migration.

--- src/main/resources/db/migration/V1__create_auth_user_tables.sql
+++ src/main/resources/db/migration/V1__create_auth_user_tables.sql
@@ -11,3 +11,21 @@ CREATE TABLE auth_providers (
     type       TEXT NOT NULL,
     CONSTRAINT uk_auth_providers_type UNIQUE (type)
 );
+
+CREATE TABLE users (
+    id           UUID        NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    first_name   TEXT        NOT NULL,
+    last_name    TEXT        NOT NULL,
+    email        TEXT        NOT NULL,
+    phone_number TEXT,
+    birth_date   DATE        NOT NULL,
+    status       TEXT        NOT NULL,
+    created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    CONSTRAINT uk_users_email UNIQUE (email)
+);
+
+CREATE TRIGGER trg_users_updated_at
+    BEFORE UPDATE ON users
+    FOR EACH ROW
+    EXECUTE FUNCTION set_updated_at();
