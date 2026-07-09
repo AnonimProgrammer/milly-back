36f3d7d73a1bf7d6d6dea54df95f255d56a52e5e
Mikayıl Quliyev <mikayilquliyev16@gmail.com>
2026-07-10 00:45:44 +0400

    feat: Add staff order pagination request and response

--- src/itest/java/com/milly/order/infrastructure/adapter/inbound/http/dto/StaffOrderListApiResponse.java
+++ src/itest/java/com/milly/order/infrastructure/adapter/inbound/http/dto/StaffOrderListApiResponse.java
@@ -1,9 +1,8 @@
 package com.milly.order.infrastructure.adapter.inbound.http.dto;
 
 import com.milly.common.web.ApiResponse;
+import com.milly.common.web.PageResponse;
 import com.milly.order.application.dto.StaffOrderResponse;
 
-import java.util.List;
-
-public class StaffOrderListApiResponse extends ApiResponse<List<StaffOrderResponse>> {
+public class StaffOrderListApiResponse extends ApiResponse<PageResponse<StaffOrderResponse>> {
 }
