b70933bf30a31585113e949a3db0d5c7e4ef0de0
Ali Safarli <alisafarli0606@gmail.com>
2026-07-12 02:36:26 +0400

    test(itest): add OrderPreparationEstimateApiResponse dto

---
+++ src/itest/java/com/milly/order/infrastructure/adapter/inbound/http/dto/OrderPreparationEstimateApiResponse.java
@@ -0,0 +1,7 @@
+package com.milly.order.infrastructure.adapter.inbound.http.dto;
+
+import com.milly.common.application.dto.ApiResponse;
+import com.milly.order.application.dto.OrderPreparationEstimateResponse;
+
+public class OrderPreparationEstimateApiResponse extends ApiResponse<OrderPreparationEstimateResponse> {
+}
