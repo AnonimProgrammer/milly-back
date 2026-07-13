e216a09030f220832c9e56804b8915730564288a
Ali Safarli <alisafarli0606@gmail.com>
2026-07-13 17:33:50 +0400

    feat: Add VenueMemberApiResponse integration DTO

---
+++ src/itest/java/com/milly/venue/infrastructure/adapter/inbound/http/dto/VenueMemberApiResponse.java
@@ -0,0 +1,7 @@
+package com.milly.venue.infrastructure.adapter.inbound.http.dto;
+
+import com.milly.common.application.dto.ApiResponse;
+import com.milly.venue.application.dto.VenueMemberResponse;
+
+public class VenueMemberApiResponse extends ApiResponse<VenueMemberResponse> {
+}
